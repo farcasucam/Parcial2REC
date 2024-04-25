9eadb41287b4936ec61a86865f6656f5e7b8185f
Francisco J. Arcas Túnez <farcas@ucam.edu>
2024-04-26 00:34:13 +0200

    Create Producto.java

---
+++ Producto.java
@@ -0,0 +1,11 @@
+class Producto {
+    public String nombre;
+    public double precio;
+    public int cantidad;
+
+    public Producto(String nombre, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+}
